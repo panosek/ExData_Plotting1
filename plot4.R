2e5928667fa02239d4d8291178b54ad7ef148e96
panosek <panosek@peak.org>
2015-06-07 13:50:39 -0700

    added comments to the scripts

--- plot4.R
+++ plot4.R
@@ -1,14 +1,24 @@
-rm("reqddata")
+#This script assumes that the household_power_consumption.txt file has already been 
+#downloaded from the web to the working directory. Each script will recreate the 
+#dataframe from the text file and will not assume that it has been previously created.
+
+# read all the data to a dataframe
 rawtable <-read.table("./household_power_consumption.txt",header=T,sep = ";", colClasses = c(rep("character",2),rep("numeric",7)),na.strings="?", stringsAsFactors = F)
 
+# add a new column 'datetime' with which is the date and time concatenated.
 rawtable <- within(rawtable, Datetime <- as.POSIXlt(paste(Date, Time),
                                                     format = "%d/%m/%Y %H:%M:%S"))
 
+#pick out the two days we are interested in and discard the rest of the data.
 reqddata <- subset(rawtable, (rawtable$Date=='1/2/2007') | (rawtable$Date == '2/2/2007'))
 
 rm("rawtable")
+
+
+#plot graphs to .png file.
+
 png("plot4.png")
-par(mfrow=c(2,2))
+par(mfrow=c(2,2)) # the plot will contain 4 graphs i.e. 2 in first row and 2 in 2nd.
 
 with(reqddata,plot(Datetime,Global_active_power,type="l",ylab = "Global active power (kilowatts)"))
 
